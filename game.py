c00a815f4f18b4f9d5efcb5f1672e0b4a2184bee
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-07-07 23:26:03 +0000

    Play again function and looping working

--- game.py
+++ game.py
@@ -1,11 +1,12 @@
 """A number-guessing game."""
 import random
-# greet player
-print("Hello friend.  Are you ready to play a guessing game?")
-# get player name
-name = input("What is your name?  ")
-print(f"Nice to meet you, {name}.")
-# choose a random number
+def greet_player():
+    # greet player
+    print("Hello friend.  Are you ready to play a guessing game?")
+    # get player name
+    name = input("What is your name?  ")
+    print(f"Nice to meet you, {name}.")
+    # choose a random number
 def play_game():
     num = random.randint(1,100)
     guess_count = 0
@@ -30,6 +31,30 @@ def play_game():
             guess_count += 1
         elif user_num == num:
             print("That is correct! You win!")
-            print(f"You guess the number in {guess_count} tries.")
+            print(f"You guessed the number in {guess_count} tries.")
             break
-play_game()
+
+def play_again():
+    print("Would you like to play again? (Y)es or (N)o?")
+    play_again = input(" > ")
+    if play_again.upper() == "Y":
+        print("Great, let's play again.")
+        return True
+    else:
+        print("Thanks for playing, goodbye!")
+        return False
+        
+
+def complete_game():
+    greet_player()
+    # play_game()
+    # play_again()
+    while True:
+        play_game()
+        play_again_choice = play_again()
+        if play_again_choice == False:
+            break
+
+
+complete_game()
+
