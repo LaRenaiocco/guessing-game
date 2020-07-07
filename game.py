905c595a77141ac1703d73da05f53cae9ff4006f
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-07-07 19:56:22 +0000

    Initial commit

---
+++ game.py
@@ -0,0 +1,11 @@
+"""A number-guessing game."""
+
+# greet player
+# get player name
+# choose a random number
+# repeat:
+# have player guess a number
+# if number is incorrect give them  a hint
+# too high or too low
+# increase number of guesses
+# if number is correct the player wins
