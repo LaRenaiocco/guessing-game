98bb174735794b7a0c778802cfdd32eb34184b7f
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-07-07 22:21:27 +0000

    Choose random integer

--- game.py
+++ game.py
@@ -1,11 +1,13 @@
 """A number-guessing game."""
-
+import random
 # greet player
 print("Hello friend.  Are you ready to play a guessing game?")
 # get player name
 name = input("What is your name?  ")
 print(f"Nice to meet you, {name}.")
 # choose a random number
+num = random.randint(1,100)
+print(num)
 # repeat:
 # have player guess a number
 # if number is incorrect give them  a hint
